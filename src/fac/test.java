aad5383d2acf7243a3baa47fddc4170a6769f864
Clau <cjacome@lucasian.com>
2017-02-03 12:28:11 -0500

    Dp V2.0
    Signed-off-by: Clau <cjacome@lucasian.com>

--- src/fac/test.java
+++ src/fac/test.java
@@ -3,23 +3,20 @@ package fac;
 import java.util.Scanner;
 
 public class test {
-<<<<<<< HEAD
- static String direc,nom,tel, producto;
-=======
- static String direc,nom,tel;
->>>>>>> refs/remotes/origin/DP3
-       static int cantidad,id;
-       static double costo;
-        static double iva, total;
-        static double tot;
-        static String personas;
+
+	static String direc,nom,tel, producto;
+    static int cantidad,id;
+    static double costo;
+    static double iva, total;
+    static double tot;
+    static String personas;
  
     public static void main(String[] args) {
    
        Scanner sc= new Scanner (System.in);
-         System.out.println("digite nombre: ");
-               nom=sc.next();
-               System.out.println("");
+        System.out.println("digite nombre: ");
+        nom=sc.next();
+        System.out.println("");
         System.out.println("digite direccion: ");
         direc=sc.next();
         System.out.println("");
@@ -29,23 +26,24 @@ public class test {
         System.out.println("digite identificacion: ");
         id=sc.nextInt();  
         System.out.println("");
-       System.out.println("digite cantidad");
+        System.out.println("digite cantidad");
         cantidad=sc.nextInt();
         System.out.println("");
-        while(cantidad<1){
-            System.out.println("digite valores validos " );
-            cantidad=sc.nextInt();
-        }
+        	while(cantidad<1){
+        		System.out.println("digite valores validos " );
+        			cantidad=sc.nextInt();
+        	}
         System.out.println("digite nombre del producto");
         producto=sc.next();
+        System.out.println("");
         System.out.println("digite costo unidad");
         costo=sc.nextInt();
         System.out.println("");
      
-        while(costo<1){
-            System.out.println("digite valores validos " );
-            costo=sc.nextInt();
-        }
+        	while(costo<1){
+        		System.out.println("digite valores validos " );
+        			costo=sc.nextInt();
+        	}
        factura();
        calcularIva();
     }
@@ -54,13 +52,13 @@ public class test {
    
 
  
-        System.out.println("su factura es " + "\n" + "\n" +" nombre: "  + nom + "\n" + "direccion: " + direc + "\n" + "telefono: " + tel + "\n" + "idenficacion: " + id + "\n" + "producto: " + producto + "\n"+ "costo unidad: "+ costo +"\n" );
+        System.out.println("su factura es " + "\n" + "\n" +" nombre: "  + nom + "\n" + "direccion: " + direc + "\n" + "telefono: " + tel + "\n" + "idenficacion: " + id + "\n" + "producto: " + producto + "\n");
  }
     public static void calcularIva(){
-         total = costo*cantidad;
+        total = costo*cantidad;
         iva=(total*19)/100;
         tot=total+iva;
-        System.out.println("costo total " + tot);
+        System.out.println("costo unidad: "+ costo +"\n" + "Iva: " + iva + "\n" + "costo total: " + tot);
 
 	}
 }
